b1699253b03b3bc3b13674cbeb2c82dd2d07d2c5
Dagdagger <schwartzshelby@yahoo.com>
2017-02-26 05:54:29 -0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
   before_action :authenticate_user!
-=======
->>>>>>> 7667f7a717d10baa326169c319072a2863a61508
 end
