ab4bb56460601a6374dec437dc87cbeb1a6ae664
Dagdagger <schwartzshelby@yahoo.com>
2017-02-26 16:56:26 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+
   resources :posts
 
   root 'posts#index'
-=======
+
 
   root 'welcome#home'
 
   resources :users
->>>>>>> 7667f7a717d10baa326169c319072a2863a61508
   
 end
